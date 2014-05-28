663341cbd7884b2300b4e19072bb43da54f7c0fd
Romain Lavoix <romain.lavoix@gmail.com>
2014-05-28 15:13:42 -0700

    adding headers in an express middleware to allow distant access

--- server-config.js
+++ server-config.js
@@ -13,8 +13,9 @@ app.configure(function(){
 	app.use(express.bodyParser()); 	// parse json data in body
 	app.use(function(req, res, next){
 		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+		res.setHeader('Access-Control-Allow-Methods', 'GET');
 		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		next();
 	});
 });
 
