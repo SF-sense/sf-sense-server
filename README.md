a21a6c22af82e4847b79e1992a9da822d1ed5504
Romain Lavoix <romain.lavoix@gmail.com>
2014-06-01 22:20:36 -0700

    securing /near

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ sf-sense accessible on http://sf-sense-server.herokuapp.com/near
 
 - rest api giving you a list of incidents in san francisco
 - /near?longitude=[longitude]&latitude=[latitude] is mandatory
-- distance (in kilometers), category (ROBBERY, ASSAULT, TRESPASS... see http://data.sfgov.org/resource/tmnf-yvry.json), time (night or day) are optionnal
+- distance (in kilometers) is optionnal
--- server-config.js
+++ server-config.js
@@ -1,6 +1,5 @@
 var db = require('./config');
 var express = require('express');
-var util = require('./lib/utility');
 var handler = require('./lib/request-handler');
 var logger = require('morgan');
 var data = require('./data-importer');
@@ -8,13 +7,21 @@ var scheduler = require('./scheduler');
 
 var app = express();
 
+var auth = express.basicAuth(function(user, pass) {
+	console.log(user + " : " + pass);
+	return user === 'sf-sense' && pass === '858F8CDDB1F324A762DBEFDC77844';
+});
+
 app.configure(function(){
-	app.use(logger());				// logging
-	app.use(express.bodyParser()); 	// parse json data in body
+	app.use(logger());
+	app.use(express.compress());
+	app.use(express.json());
+	app.use(express.urlencoded());
 	app.use(function(req, res, next){
 		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Methods', 'GET');
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		res.setHeader("Access-Control-Allow-Credentials", "true");
 		next();
 	});
 });
@@ -22,17 +29,8 @@ app.configure(function(){
 // refresh data when opening a new connection
 data.importData();
 
-app.get('/near', handler.getNear);
+app.get('/near', auth, handler.getNear);
 
 scheduler.schedule();
 
 module.exports = app;
-
-/**
-
-	res.set('access-control-allow-origin', '*');
-	res.set('access-control-allow-methods', 'GET');
-	res.set('access-control-allow-headers', 'origin, content-type, accept');
-
-
-**/
